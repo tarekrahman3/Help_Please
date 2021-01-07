83a37c619558a8df714d64ea4463a20a71c8ca52
tarekrahman3 <44648395+tarekrahman3@users.noreply.github.com>
2021-01-07 22:15:36 +0600

    Update bs4_seleniumChrome.py

--- bs4_seleniumChrome.py
+++ bs4_seleniumChrome.py
@@ -1,35 +1,44 @@
-from selenium import webdriver
 import os
 import time
-#from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import re
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+text = []
+url = []
+rows = []
 
 options = Options()
-#options.headless = True
+options.headless = True
 driver = webdriver.Chrome(options=options, executable_path='chromedriver')
 
-Search_Strings = ["red", "green", "blue", "purple"]
-i = 0
+Search_Strings = [
+ 'LinkedIn',
+ 'Crunchbase'
+]
+for string in Search_Strings:
+    time.sleep(3)
+    driver.get('http://www.google.com')
+    time.sleep(4)
+    que = driver.find_element_by_xpath("//input[@name='q']")
+    que.send_keys(string)
+    time.sleep(4)
+    que.send_keys(Keys.RETURN)
+    time.sleep(4)
+    col__1 = driver.find_element_by_id('rso').find_element_by_xpath('.//h3/span').text
+    col_1 = 'Title:', col__1
+    text.append(col_1)
+    col__2 = driver.find_element_by_id('rso').find_element_by_xpath('.//a').get_attribute('href')
+    col_2 = 'Website:', col__2
+    url.append(col_2)
+    row = col_1, col_2
+    rows.append(row)
 
-while i < len(Search_Strings):
+for data in rows:
+    print(data)
 
- driver.get("http://www.google.com")  
- time.sleep(3)
- que=driver.find_element_by_xpath("//input[@name='q']")
- que.send_keys(Search_Strings[i])
- time.sleep(3)
- que.send_keys(Keys.RETURN)
- time.sleep(3)
- 
- """
- source = driver.page_source
- print (source)
- soup=BeautifulSoup(source, 'lxml')
- element=soup.find_all('h')
- print (soup)
- """
-i += 1
+driver.quit()
